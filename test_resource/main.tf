78b03b40374d120258dc2cf7a37f0d541ce53b3c
dannielshalev <44364077+dannielshalev@users.noreply.github.com>
2024-07-28 10:51:00 +0300

    move the resource to the relevat project

---
+++ test_resource/main.tf
@@ -0,0 +1,18 @@
+module "s3_cloudfront" {
+  source = "../terraform-s3-cloudfront-module"
+
+  aws_region             = "us-east-1"
+  bucket_name            = "your-bucket-name"
+  versioning_enabled     = true
+  cloudfront_role_name   = "cloudfront-access-role"
+  cloudfront_policy_name = "cloudfront-access-policy"
+  cloudfront_oai_comment = "Origin Access Identity for CloudFront"
+  cloudfront_comment     = "CloudFront distribution for S3 bucket"
+  default_root_object    = "index.html"
+  default_ttl            = 3600
+  max_ttl                = 86400
+  tags                   = {
+    Environment = "production"
+    Project     = "example"
+  }
+}
